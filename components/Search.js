34137394bb91b53c5c34e715babeca8178c0caf3
Kevin Hallett <tauren125@gmail.com>
2019-01-30 15:41:38 -0600

    add search

---
+++ components/Search.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { TextInput, StyleSheet, Dimensions } from 'react-native';
+
+export class Search extends Component {
+  render() {
+    return (
+      <TextInput
+        style={styles.input}
+        placeholder={this.props.placeholder}
+        onChangeText={this.props.onChange}
+        value={this.props.value}
+      ></TextInput>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  input: {
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: '#ff0',
+    paddingTop: 6,
+    paddingBottom: 6,
+    paddingLeft: 14,
+    paddingRight: 14,
+    width: Dimensions.get('window').width * .65,
+    backgroundColor: '#777779',
+    color: '#fff',
+  }
+});
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,8 +3,12 @@ import {
   ScrollView,
   StyleSheet,
   Text,
-  View
+  View,
+  Platform,
+  Button
 } from 'react-native';
+
+import { Search } from '../components/Search';
 
 
 export default class HomeScreen extends React.Component {
@@ -12,12 +16,30 @@ export default class HomeScreen extends React.Component {
     header: null,
   };
 
+  state = {
+    query: ''
+  }
+
+  handleSearchChange = query => {
+    this.setState({ query })
+  }
+
+  handleSearch = () => {
+    const { query } = this.state;
+    console.log(query);
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
           <View style={styles.welcomeContainer}>
             {this.maybeDisplayDevMode()}
+            <Text style={styles.title}>Overstats</Text>
+            <View style={styles.searchContainer}>
+              <Search placeholder="Enter a username" value={this.state.query} onChange={this.handleSearchChange}></Search>
+              <Button style={styles.searchButton} title="Search" onPress={this.handleSearch}></Button>
+            </View>
           </View>
         </ScrollView>
 
@@ -28,7 +50,7 @@ export default class HomeScreen extends React.Component {
   maybeDisplayDevMode() {
     if (__DEV__) {
       return (
-        <Text>DEV MODE</Text>
+        <Text style={styles.devModeText}>DEV MODE</Text>
       )
     }
   }
@@ -38,7 +60,7 @@ export default class HomeScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff'
+    backgroundColor: '#262629'
   },
   contentContainer: {
     paddingTop: 30
@@ -47,5 +69,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginTop: 10,
     marginBottom: 20
+  },
+  searchContainer: { flex: 1, flexDirection: 'row' },
+  devModeText: {
+    color: '#009ae4'
+  },
+  title: {
+    fontSize: 75,
+    color: '#ff9c00',
+    marginBottom: 10,
+    fontFamily: Platform.select({
+      ios: 'Helvetica Neue',
+      android: 'Roboto'
+    }),
+  },
+  searchButton: {
+    marginTop: 50,
   }
 });
